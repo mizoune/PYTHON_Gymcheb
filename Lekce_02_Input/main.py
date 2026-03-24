f67376e379416d981bcb0d6453f88e47fc654ab0
mizoune <mizoune@gymcheb.cz>
2026-03-24 08:14:25 +0100

    Update main.py

--- Lekce_02_Input/main.py
+++ Lekce_02_Input/main.py
@@ -2,15 +2,28 @@
 # ÚLOHA 1: Registrace (jméno a město)
 # ==========================================
 # Sem napiš svůj kód:
+jmeno = input("Zadej své jméno: ")
+barva = input("Zadej svou oblíbenou barvu: ")
 
+print(f"Ahoj {jmeno}, tvoje oblíbená barva je {barva}. To je skvělá volba!")
 
 # ==========================================
 # ÚLOHA 2: Jednoduchá kalkulačka (součet)
 # ==========================================
 # Sem napiš svůj kód:
+cislo1 = int(input("Zadej první číslo: "))
+cislo2 = int(input("Zadej druhé číslo: "))
 
+soucet = cislo1 + cislo2
+
+print(f"Součet je: {soucet}")
 
 # ==========================================
 # ÚLOHA 3: Výpočet psího věku (věk * 7)
 # ==========================================
 # Sem napiš svůj kód:
+vek = int(input("Kolik je ti let? "))
+
+psi_vek = vek * 7
+
+print(f"V psích letech by ti bylo: {psi_vek} let. Haf!")
